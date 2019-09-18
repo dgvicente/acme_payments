fac99712159ab5b93bf9cab23465dc635fe588b0
Diana <dianagv@gmail.com>
2019-09-18 00:03:09 -0500

    adds flow skeleton to main file

--- acme_payments.py
+++ acme_payments.py
@@ -1,5 +1,13 @@
 def main():
-    print("Acme payments")
+    print 'Welcome to the ACME payments platform!'
+    print '--------------------------------------'
+    config_file = raw_input("Please provide a file path for payment rates by hour or leave it blank to use default: ")
+    print config_file if config_file else 'DEFAULT'
+    questions = raw_input("Now, please provide your questions file or leave it blank to use default: ")
+    print questions if questions else 'DEFAULT'
+    print 'The answers you were looking for:'
+    print '---------------------------------'
+    print 'The amount to pay RENE is: 215 USD'
 
 
 if __name__ == "__main__":
